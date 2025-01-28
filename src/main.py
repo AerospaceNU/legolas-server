8804272fea5b211b61f5ef621057823b7370211a
SamHaggans <sam@samhaggans.com>
2025-01-27 19:59:16 -0500

    Add legolas_common submodule and basic client/server implementation

--- src/main.py
+++ src/main.py
@@ -1,7 +1,49 @@
+import time
+from queue import Queue
+
+from legolas_common.src.packet_types import (
+    BROADCAST_DEST,
+    Packet,
+    PacketAddress,
+    PacketType,
+)
+from legolas_common.src.socket_server import SocketServer
+
+
 def main() -> None:
     """Program entrypoint"""
-    print("Main!")
-    return
+
+    outgoing_data: Queue[Packet] = Queue()
+    received_data: Queue[Packet] = Queue()
+
+    server = SocketServer("127.0.0.1", 12348, outgoing_data, received_data)
+    prev_update_time = time.time()
+    addresses: set[PacketAddress] = set()
+    server.run()
+    try:
+        while True:
+            if time.time() - prev_update_time > 1:
+                outgoing_data.put(Packet(PacketType.CONTROL, BROADCAST_DEST, {"hello": "world"}))
+                prev_update_time = time.time()
+                for i, address in enumerate(addresses):
+                    outgoing_data.put(Packet(PacketType.CONTROL, address, {"hello": f"client {i}"}))
+
+            if not received_data.empty():
+                msg = received_data.get()
+                addresses.add(msg.packet_address)
+                print(f"From {msg.packet_address}: ", end="")
+                if msg.packet_type == PacketType.INTERNAL:
+                    print(f"Received internal: {msg.payload}")
+                elif msg.packet_type == PacketType.CONTROL:
+                    print(f"Received control: {msg.payload}")
+                elif msg.packet_type == PacketType.IMAGE:
+                    print("Received image")
+                else:
+                    print("Received ack: {msg.payload}")
+            time.sleep(0.001)
+    except KeyboardInterrupt:
+        server.shutdown()
+        print("Goodbye!")
 
 
 if __name__ == "__main__":
---
+++ src/sample_client.py
@@ -0,0 +1,27 @@
+import time
+from queue import Queue
+
+from legolas_common.src.packet_types import BROADCAST_DEST, Packet, PacketType
+from legolas_common.src.socket_client import SocketClient
+
+if __name__ == "__main__":
+    outgoing_data: Queue[Packet] = Queue()
+    received_data: Queue[Packet] = Queue()
+    client = SocketClient("127.0.0.1", 12348, outgoing_data, received_data)
+    prev_update_time = 0.0
+
+    client.run()
+    try:
+        while True:
+            if time.time() - prev_update_time > 10:
+                outgoing_data.put(
+                    Packet(
+                        PacketType.CONTROL,
+                        BROADCAST_DEST,  # This doesn't actually matter
+                        {"hello": "server", "time": time.time()},
+                    )
+                )
+                prev_update_time = time.time()
+    except KeyboardInterrupt:
+        client.shutdown()
+        print("Shutting down client main")
